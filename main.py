df1c5577b161844b4404407966321523450d9c9c
Remil <remil@remilia.pl>
2022-09-29 16:06:08 +0200

    a

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+
+import asyncio
 import discord
+from discord.ext import tasks
+import tohu
+import datetime as dt
+
+
+
+
+
+
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -14,6 +25,7 @@ async def on_ready():
     guilg = await bot.fetch_guild(647798243207544842)
     bot.tree.copy_global_to(guild=guilg)
     await bot.tree.sync(guild=guilg)
+    msg1.start()
 
 
 @bot.event
@@ -24,7 +36,100 @@ async def on_message(ctx):
 async def ping(interaction: discord.Interaction) -> None:
     await interaction.response.send_message("ping")
 
+@bot.tree.command(name=f'upload', description=f'Przeslij swoj replay')
+async def upload(interaction: discord.Interaction) -> None:
 
+    ID = interaction.channel.id
+    channel = bot.get_channel(int(ID))
+
+    message = await channel.fetch_message(channel.last_message_id)
+
+
+    if str(message.attachments) == "[]":  # Checks if there is an attachment on the message
+        return
+    else:  # If there is it gets the filename from message.attachments
+        split_v1 = str(message.attachments).split("filename='")[1]
+        filename = str(split_v1).split("' ")[0]
+        if filename.endswith(".rpy"):  # Checks if it is a .csv file
+            await message.attachments[0].save(fp=f'replays/{interaction.user.id}.rpy')
+            x = tohu.parse(interaction.user.id)
+            await interaction.response.send_message(f'{x}')
+        else:
+            await interaction.response.send_message(f'To nie jest plik replayu')
+
+@bot.tree.command(name=f'weekstats', description=f'Pokazuje statystyki tego tygodnia')
+async def weekstats(interaction: discord.Interaction) -> None:
+    f = open(f'week', 'r')
+    week = eval(f.read())
+    f.close()
+    x = f'Weekly challange to: {week["g"]} {week["t"]} {week["m"]} {week["c"]}\n'
+    scores = tohu.sta()
+    for y in scores:
+        us = bot.get_user(int(y[0]))
+        x += f'{us.display_name} score: {y[1]} stage:{y[2]}\n'
+    await interaction.response.send_message(f'{x}')
+
+@bot.tree.command(name=f'getkey', description=f'Pokazuje klucz którym masz podpisać replay')
+async def getkey(interaction: discord.Interaction) -> None:
+    id = interaction.user.id
+    u = tohu.floady(id)
+    klucz = u.akey
+    x = f'Twój klucz to {klucz}'
+    await interaction.response.send_message(f'{x}')
+
+@bot.tree.command(name=f'register', description=f'Rejestruje konto do weekly challangu')
+async def register(interaction: discord.Interaction) -> None:
+    id = interaction.user.id
+    x = tohu.create(id)
+    await interaction.response.send_message(f'{x}')
+
+@bot.tree.command(name=f'staty', description=f'Pokazuje statystyki')
+async def staty(interaction: discord.Interaction) -> None:
+    scores = tohu.staty()
+    x = f''
+    for y in scores:
+        us = bot.get_user(int(y[0]))
+        x += f'{us.display_name} Liczba punktow:{y[1]}\n'
+    await interaction.response.send_message(f'{x}')
+
+@commands.is_owner()
+@bot.tree.command(name=f'endweek', description=f'konczytydzien (debug)')
+async def endweek(interaction: discord.Interaction) -> None:
+    await endw()
+    await interaction.response.send_message(f'juz')
+
+# 7 days => 24 hour * 7 days = 168
+@tasks.loop(hours=168)
+async def msg1():
+    await endw()
+
+
+@msg1.before_loop
+async def before_msg1():
+    # loop the whole 7 day (60 sec 60 min 24 hours 7 days)
+    for _ in range(60*60*24*7):
+        if dt.datetime.utcnow().strftime("%H:%M UTC %w") == "12:00 UTC 7":
+
+            print('It is time')
+            return
+
+        # wait some time before another loop. Don't make it more than 60 sec or it will skip
+        await asyncio.sleep(30)
+async def endw():
+
+    wyg = (tohu.endweek())
+    if type(wyg) is type(0):
+        g = await bot.fetch_guild(647798243207544842)
+        c = await g.fetch_channel(790562786992193548)
+        await c.send(f'Weekly danmaku challange wygrał: <@!{wyg}>')
+        x = tohu.newweek()
+        await c.send(f'Nowy weekly challange to: {x["g"]} {x["t"]} {x["m"]} {x["c"]}')
+    else:
+        g = await bot.fetch_guild(647798243207544842)
+        c = await g.fetch_channel(790562786992193548)
+        await c.send(f'Weekly danmaku challange wygrał: Nikt!')
+        x = tohu.newweek()
+        await c.send(f'Nowy weekly challange to: {x["g"]} {x["t"]} {x["m"]} {x["c"]}')
 
 
 
@@ -33,5 +138,6 @@ async def ping(interaction: discord.Interaction) -> None:
 
 with open(f'token.txt', 'r') as f:
     token = f.read()
+
 
 bot.run(token)
--- tohu.py
+++ tohu.py
@@ -1,7 +1,202 @@
+import os
+import random
 import threp
+import pickle
 
-tr = threp.THReplay(f'th7_02.rpy')
-print(tr.getBaseInfoDic())
-print(tr.getStageScore())
-print(tr.getX())
+def gen():
+    i = f''
+    for x in range(0, 6):
+        i += str(random.randint(0,9))
+    return i
+
+class Thser:
+    def __init__(self, id):
+        self.id = id
+        self.lp = 0
+        self.lastscore = 0
+        self.submit = False
+        self.stage = 1
+        self.akey = gen()
+
+
+
+def save(u):
+    f = open(f'users/{u.id}.plik', 'wb')
+    pickle.dump(u, f)
+    f.close()
+
+def floady(id):
+    f = open(f'users/{id}.plik', 'rb')
+    u = pickle.load(f)
+    f.close()
+    return u
+
+def create(id):
+    u = Thser(id)
+    save(u)
+    return f'Twój klucz to {u.akey}'
+def parse(id):
+    try:
+        u = floady(id)
+
+
+        h = open(f'replays/{id}.rpy', 'rb')
+        data = h.read()
+        game = str(data[0:3])[2:5]
+        game = game.replace('R', '').lower()
+        tr = threp.THReplay(f'replays/{id}.rpy')
+        f = open(f'week', 'r')
+        week = eval(f.read())
+        f.close()
+
+        check = False
+        if len(game) < 3:
+            if game[1] in week['g']:
+                check = True
+        else:
+            if game[1:2] in week['g']:
+                check = True
+        ret = f'Niepoprawny plik replay/Nie spelnione warunki zadania'
+        if check and tr.getPlayer() == u.akey:
+            repl = tr.getBaseInfoDic()
+            score = tr.getStageScore()
+            character = repl['character'] + repl['shottype']
+            if week['m'] == repl['rank'].lower():
+                if character == week['c']:
+
+                    if week['t'] == '1cc':
+                        if week['g'] == 'th06':
+                            if len(score) == 5:
+                                u.stage = len(score)
+                                u.lastscore = score[-1]
+                                u.submit = True
+                                save(u)
+                                ret = f'Zapisano score {u.lastscore} stage: {u.stage}'
+                        else:
+                            if repl['stage'] == 'All':
+                                u.stage = len(score)
+                                u.lastscore = score[-1]
+                                u.submit = True
+                                save(u)
+                                ret = f'Zapisano score {u.lastscore} stage: {u.stage}'
+
+                    elif week['t'] == 'nobomb' and tr.getX() == []:
+                        u.stage = len(score)
+                        u.lastscore = score[-1]
+                        u.submit = True
+                        save(u)
+                        ret = f'Zapisano score {u.lastscore} stage: {u.stage}'
+
+                    elif week['t'] == 'noshoot' and tr.getZ() == []:
+                        u.stage = len(score)
+                        u.lastscore = score[-1]
+                        u.submit = True
+                        save(u)
+                        ret = f'Zapisano score {u.lastscore} stage: {u.stage}'
+
+                    elif week['t'] == 'nofocus' and tr.getShift() == []:
+                        u.stage = len(score)
+                        u.lastscore = score[-1]
+                        u.submit = True
+                        save(u)
+                        ret = f'Zapisano score {u.lastscore} stage: {u.stage}'
+            save(u)
+        return ret
+    except:
+        return f'Dupa totalna'
+
+
+
+def newweek():
+    mode = ['easy', 'normal', 'hard', 'lunatic', 'extra']
+    game = ['th06', 'th07', 'th08', 'th10', 'th11', 'th12', 'th13', 'th14', 'th15', 'th16', 'th17', 'th18']
+    task = ['1cc', 'nobomb', 'noshoot', 'nofocus']
+    chara = {
+    'th06': ['ReimuA', 'ReimuB', 'MarisaA', 'MarisaB'],
+    'th07': ['ReimuA', 'ReimuB', 'MarisaA', 'MarisaB', 'SakuyaA', 'SakuyaB'],
+    'th08': ['Reimu', 'Sakuya', 'Yukari', 'Remilia', 'Marisa', 'Alice', 'Youmu', 'Yuyuko', 'Rm & Yk', 'Ms & Al',
+             'Sk & Rr', 'Ym & Yy'],
+    'th10': ['ReimuA', 'ReimuB', 'ReimuC', 'MarisaA', 'MarisaB', 'MarisaC'],
+    'th11': ['ReimuA', 'ReimuB', 'ReimuC', 'MarisaA', 'MarisaB', 'MarisaC'],
+    'th12': ['ReimuA', 'ReimuB', 'MarisaA', 'MarisaB', 'SanaeA', 'SanaeB'],
+    'th13': ['Reimu', 'Marisa', 'Sanae', 'Youmu'],
+    'th14': ['ReimuA', 'ReimuB', 'MarisaA', 'MarisaB', 'SakuyaA', 'SakuyaB'],
+    'th15': ['Reimu', 'Marisa', 'Sanae', 'Reisen'],
+    'th16': ['ReimuSpring', 'ReimuSummer', 'ReimuAutumn', 'ReimuWinter', 'MarisaSpring', 'MarisaSummer', 'MarisaAutumn',
+             'MarisaWinter', 'CirnoSpring', 'CirnoSummer', 'CirnoAutumn', 'CirnoWinter', 'AyaSpring', 'AyaSummer',
+             'AyaAutumn', 'AyaWinter'],
+    'th17': ['ReimuWolf', 'ReimuOtter', 'ReimuEagle', 'MarisaWolf', 'MarisaOtter', 'MarisaEagle', 'YoumuWolf',
+             'YoumuOtter', 'YoumuEagle'],
+    'th18': ['Reimu', 'Marisa', 'Sakuya', 'Sanae']}
+
+    g = random.choice(game)
+    m = random.choice(mode)
+    t = random.choice(task)
+    c = random.choice(chara[g])
+    if g == 'th07':
+        if m == 'extra':
+            m = random.choice(['extra', 'phantasm'])
+    elif g == 'th16':
+        t = random.choice(['1cc', 'nobomb', 'noshoot', 'nofocus', 'norelease'])
+
+    week = {'g': g, 'm': m, 't': t, 'c': c}
+    with open(f'week', 'w') as f:
+        f.write(str(week))
+    return week
+
+
+def sta():
+    files = os.listdir(f'users')
+    scores = []
+
+    for x in files:
+        fikle = x.split('.')
+        tab = []
+        us = floady(fikle[0])
+        if us.submit:
+            tab.append(us.id)
+            tab.append(us.lastscore)
+            tab.append(us.stage)
+
+            scores.append(tab.copy())
+    y = sorted(scores, key = lambda x: x[1], reverse=True)
+    return y
+
+def endweek():
+    scores = sta()
+    if scores != []:
+        files = os.listdir(f'users')
+        for x in files:
+            fikle = x.split('.')
+            us = floady(fikle[0])
+
+            if not us.submit:
+                if us.lp > 0:
+                    us.lp -= 5
+                    us.submit = False
+                    save(us)
+            else:
+                us.lp += 5
+                us.submit = False
+                save(us)
+        us = floady(int(scores[0][0]))
+        us.lp += 5
+        save(us)
+        return us.id
+    return f'Nikt nie wygral'
+
+def staty():
+    files = os.listdir(f'users')
+    scores = []
+
+    for x in files:
+        fikle = x.split('.')
+        tab = []
+        us = floady(fikle[0])
+        tab.append(us.id)
+        tab.append(us.lp)
+        scores.append(tab.copy())
+
+    y = sorted(scores, key = lambda x: x[1], reverse=True)
+    return y
 
