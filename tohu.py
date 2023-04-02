b9bd449e0028a042beaf411440867ccd20b6c551
RemilSca <kamildudek2001@gmail.com>
2023-04-02 12:54:44 +0200

    rlynig

--- tohu.py
+++ tohu.py
@@ -107,7 +107,7 @@ def parse(id):
 
 def newweek():
     mode = ['easy', 'normal', 'hard', 'lunatic', 'extra']
-    game = ['th06', 'th07', 'th08', 'th10', 'th11', 'th12', 'th13', 'th14', 'th15', 'th16', 'th17' 'th18']
+    game = ['th06', 'th07', 'th08', 'th10', 'th11', 'th12', 'th13', 'th14', 'th15', 'th16', 'th17', 'th18']
     task = ['1cc', 'nobomb', 'nofocus']
     chara = {
     'th06': ['ReimuA', 'ReimuB', 'MarisaA', 'MarisaB'],
